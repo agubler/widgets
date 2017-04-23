ad4cc4d352039382f0755eadf29983a507d8fba2
Anthony Gubler <agubler@sitepen.com>
2017-04-23 21:29:39 +0100

    support sub menus

--- src/basic-menu/Menu.ts
+++ src/basic-menu/Menu.ts
@@ -1,108 +1,218 @@
-import { WidgetBase } from '@dojo/widget-core/WidgetBase';
-import { DNode, WidgetProperties } from '@dojo/widget-core/interfaces';
+import { WidgetBase, afterRender } from '@dojo/widget-core/WidgetBase';
+import { Constructor, DNode, WidgetProperties } from '@dojo/widget-core/interfaces';
 import { ThemeableMixin, theme } from '@dojo/widget-core/mixins/Themeable';
-import { v, w } from '@dojo/widget-core/d';
+import { v, isWNode, isHNode } from '@dojo/widget-core/d';
 import { Keys } from '../common/util';
 
 import * as css from './styles/basicMenu.m.css';
 
 export interface ContainerProperties extends WidgetProperties {
-}
-
-export interface ItemWrapperProperties extends WidgetProperties {
-	key: string;
-	onFocusItem: Function;
-	activeItem: boolean;
-	scheduleItemFocus: boolean;
+	open?: boolean;
+	activeItem: number;
+	onFocus: Function;
+	role?: string;
 }
 
 export interface ItemProperties extends WidgetProperties {
 	title: string;
+	action?: Function;
 	onClick?: Function;
 }
 
-export class Container extends WidgetBase<ContainerProperties> {
-
-	private _activeItem = 0;
-	private _scheduleFocus = false;
+export class Container extends FocusManagerMixin(WidgetBase)<ContainerProperties> {
 
 	private _keyDown(event: KeyboardEvent) {
+		let nextActiveItem;
 		switch (event.which) {
 			case Keys.Up:
 				event.preventDefault();
 				event.stopPropagation();
-				this._activeItem = this._activeItem === 0 ? this.children.length - 1 : this._activeItem - 1;
-				this._scheduleFocus = true;
-				this.invalidate();
+				nextActiveItem = this.properties.activeItem === 0 ? this.children.length - 1 : this.properties.activeItem - 1;
+				this.scheduleFocus(true);
+				this.properties.onFocus(nextActiveItem);
 				break;
 			case Keys.Down:
 				event.preventDefault();
 				event.stopPropagation();
-				this._activeItem = this._activeItem === this.children.length - 1 ? 0 : this._activeItem + 1;
-				this._scheduleFocus = true;
-				this.invalidate();
+				nextActiveItem = this.properties.activeItem === this.children.length - 1 ? 0 : this.properties.activeItem + 1;
+				this.scheduleFocus(true);
+				this.properties.onFocus(nextActiveItem);
 				break;
 		}
 	}
 
-	private _onFocusItem(item?: number) {
-		this._scheduleFocus = false;
-		if (item !== undefined) {
-			this._activeItem = item;
-			this.invalidate();
-		}
-	}
-
 	protected render(): DNode {
-		const wrappedItems = this.children.map((child, i) => {
-			const activeItem = this._activeItem === i;
-			const scheduleItemFocus = this._scheduleFocus && activeItem;
-			return w(ItemWrapper, { key: String(i), onFocusItem: this._onFocusItem, scheduleItemFocus, activeItem }, [ child ]);
-		});
+		const { open = true } = this.properties;
 
-		return v('ul', { key: this.properties.key, onkeydown: this._keyDown }, wrappedItems);
+		return open ?
+			v('ul', {
+				id: this.properties.key,
+				key: this.properties.key,
+				onkeydown: this._keyDown,
+				'aria-labelledby': (<any> this.properties).labelledBy,
+				role: this.properties.role || 'menubar'
+			}, this.children) : null;
 	}
 }
 
 @theme(css)
-export class ItemWrapper extends ThemeableMixin(WidgetBase)<ItemWrapperProperties> {
+export class Item extends FocusSchedulerMixin(ThemeableMixin(WidgetBase))<any> {
 
-	protected onElementCreated(element: HTMLElement, key: string) {
-		if (this.properties.scheduleItemFocus) {
-			element.focus();
-			this.properties.onFocusItem();
-		}
-	}
-
-	protected onElementUpdated(element: HTMLElement, key: string) {
-		if (this.properties.scheduleItemFocus) {
-			element.focus();
-			this.properties.onFocusItem();
-		}
-	}
-
-	protected onClick(event: MouseEvent) {
+	// TODO update the index and call the action
+	private onClick(event: MouseEvent) {
 		event.stopPropagation();
-		this.properties.onFocusItem(parseInt(this.properties.key, 10));
+		this.properties.onFocus(this.properties.index);
+		this.properties.action && this.properties.action();
 	}
 
+	// TODO seperate orientation key module perhaps?
+	private onKeyDown(event: KeyboardEvent) {
+		switch (event.which) {
+			case Keys.Space:
+				event.preventDefault();
+				event.stopPropagation();
+				this.properties.action && this.properties.action();
+			break;
+			case Keys.Right:
+				if (this.properties.action) {
+					event.preventDefault();
+					event.stopPropagation();
+					this.properties.action(true);
+				}
+			break;
+			case Keys.Left:
+			case Keys.Escape:
+				if (this.properties.action) {
+					event.preventDefault();
+					event.stopPropagation();
+					this.properties.retrieveFocus();
+					this.properties.action(false);
+				}
+			break;
+		}
+	}
+
+	protected getFocusTarget() {
+		return `item-label-${this.properties.key}`;
+	}
+
+	// TODO this assumes that the children is a Container! I think this is better off to be a property...
 	protected render(): DNode {
 		return v('li', {
-			key: this.properties.key,
+			id: this.properties.key,
 			onclick: this.onClick,
-			tabIndex: this.properties.activeItem ? 0 : -1,
-			classes: this.classes(css.item)
-		}, this.children);
+			onkeydown: this.onKeyDown,
+			key: this.properties.key,
+			classes: this.classes(css.item),
+			role: 'none'
+		}, [
+			v('div', {
+				tabIndex: this.properties.activeItem ? 0 : -1,
+				classes: this.classes(css.item),
+				key: `item-label-${this.properties.key}`,
+				role: 'menuitem'
+			}, [ this.properties.title ]),
+			...this.children
+		]);
+	}
+
+	// TODO don't like this
+	@afterRender()
+	protected a11y(result: DNode) {
+		if (isHNode(result) || isWNode(result)) {
+			if (result.children && result.children.length === 2) {
+				(<any> result.children[1]).properties.labelledBy = this.properties.key;
+				(<any> result.children[1]).properties.autoFocus = true;
+				(<any> result.children[1]).properties.role = 'menu';
+				(<any> result.children[0]).properties['aria-controls'] = (<any> result.children[1]).properties.key;
+			}
+		}
+
+		return result;
 	}
 }
 
-export class Item extends ThemeableMixin(WidgetBase)<ItemProperties> {
+interface FocusManagerMixin {
+	scheduleFocus(value: boolean): void;
+}
 
-	private _onClick(event: MouseEvent) {
-		this.properties.onClick && this.properties.onClick();
+function FocusManagerMixin<T extends Constructor<WidgetBase<any>>>(Base: T): T & Constructor<FocusManagerMixin> {
+
+	class FocusManager extends Base {
+
+		private _scheduleFocus = false;
+
+		public scheduleFocus(value: boolean) {
+			this._scheduleFocus = value;
+		}
+
+		@afterRender()
+		protected decorateFocus(result: DNode) {
+			if (!(typeof result === 'string') && result !== null) {
+				const { _scheduleFocus, onFocus, retrieveFocus, properties: { activeItem, autoFocus } } = this;
+				result.children.forEach((node: DNode, i) => {
+					if (isWNode(node)) {
+						const active = i === activeItem;
+						(<any> node.properties).index = i;
+						(<any> node.properties).activeItem = active;
+						(<any> node.properties).retrieveFocus = retrieveFocus;
+						(<any> node.properties).autoFocus = autoFocus && active;
+						(<any> node.properties).scheduleFocus = _scheduleFocus && active;
+						(<any> node.properties).onFocus = onFocus;
+					}
+				});
+			}
+			return result;
+		}
+
+		private onFocus(item?: number) {
+			this.scheduleFocus(false);
+			if (item !== undefined) {
+				this.properties.onFocus(item);
+			}
+		}
+
+		private retrieveFocus() {
+			this.scheduleFocus(true);
+		}
 	}
 
-	protected render(): DNode {
-		return v('div', { onclick: this._onClick, key: this.properties.key }, [ this.properties.title, ...this.children ]);
+	return FocusManager;
+}
+
+let createFocusCount = 0;
+let updatedFocusCount = 0;
+
+function FocusSchedulerMixin<T extends Constructor<WidgetBase<any>>>(Base: T): T {
+
+	class FocusScheduler extends Base {
+
+		protected onElementCreated(element: HTMLElement, key: string) {
+			if (key === this.getFocusTarget()) {
+				const { onFocus, scheduleFocus = false } = this.properties;
+				if (scheduleFocus || this.properties.autoFocus) {
+					console.log('focus from created', ++createFocusCount);
+					element.focus();
+					onFocus();
+				}
+			}
+		}
+
+		protected onElementUpdated(element: HTMLElement, key: string) {
+			if (key === this.getFocusTarget()) {
+				const { onFocus, scheduleFocus = false } = this.properties;
+				if (scheduleFocus) {
+					console.log('focus from updated', ++updatedFocusCount);
+					element.focus();
+					onFocus();
+				}
+			}
+		}
+
+		protected getFocusTarget() {
+			return this.properties.key;
+		}
 	}
+
+	return FocusScheduler;
 }
--- src/basic-menu/example/index.ts
+++ src/basic-menu/example/index.ts
@@ -7,22 +7,60 @@ import { Container, Item } from '../Menu';
 
 const AppBase = StatefulMixin(WidgetBase);
 
+let menuOneActiveItem = 0;
+let menuTwoActiveItem = 0;
+let subMenuActiveItem = 0;
+
+function onMenuOneItemChange(value: number) {
+	menuOneActiveItem = value;
+	projector.invalidate();
+}
+
+function onMenuTwoItemChange(value: number) {
+	menuTwoActiveItem = value;
+	projector.invalidate();
+}
+
+function onSubMenutemChange(value: number) {
+	subMenuActiveItem = value;
+	projector.invalidate();
+}
+
+let subMenuOpenState = false;
+
+function toggleSubMenu(state?: boolean) {
+	subMenuOpenState = state !== undefined ? state : !subMenuOpenState;
+	subMenuActiveItem = 0;
+	projector.invalidate();
+}
+
 export class App extends AppBase<WidgetProperties> {
 
 	render() {
 		return v('div', [
-			w(Container, { key: '1' }, [
-				w(Item, { key: '1', title: 'item1' } ),
-				w(Item, { key: '2', title: 'item2' } ),
-				w(Item, { key: '3', title: 'item3' } ),
-				w(Item, { key: '4', title: 'item4' } )
+			w(Container, { key: 'container-1', activeItem: menuOneActiveItem, onFocus: onMenuOneItemChange }, [
+				w(Item, { key: 'container-1-item-1', title: 'item1' } ),
+				w(Item, { key: 'container-1-item-2', title: 'item2' } ),
+				w(Item, { key: 'container-1-item-3', title: 'item3' } ),
+				w(Item, { key: 'container-1-item-4', title: 'item4' } )
 			]),
-			w(Container, { key: '2' }, [
-				w(Item, { key: '1', title: 'item5' } ),
-				w(Item, { key: '2', title: 'item6' } ),
-				v('div', [ 'item' ]),
-				w(Item, { key: '3', title: 'item7' } ),
-				w(Item, { key: '4', title: 'item8' } )
+			w(Container, { key: 'container-2', activeItem: menuTwoActiveItem, onFocus: onMenuTwoItemChange }, [
+				w(Item, { key: 'container-2-item-1', title: 'item5' } ),
+				w(Item, { key: 'container-2-item-2', title: 'item6' } ),
+				w(Item, { key: 'container-2-item-3', title: 'item7' } ),
+				w(Item, { key: 'container-2-item-4', title: 'item8' } ),
+				w(Item, { key: 'container-2-item-5', title: 'sub menu', action: toggleSubMenu }, [
+					w(Container, {
+						key: 'container-3',
+						open: subMenuOpenState,
+						activeItem: subMenuActiveItem,
+						onFocus: onSubMenutemChange
+					}, [
+						w(Item, { key: 'container-3-item-1', title: 'sub menu item 1' } ),
+						w(Item, { key: 'container-3-item-2', title: 'sub menu item 2' } ),
+						w(Item, { key: 'container-3-item-3', title: 'sub menu item 3' } )
+					])
+				])
 			])
 		]);
 	}
