d51358e5f151afd592b83ee71bf2f21445ced350
Anthony Gubler <agubler@sitepen.com>
2019-11-07 10:11:32 +0000

    update deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cp ./now.json ./output/dist
 
-name="dojo.widgets"
+name="widgets.dojo.io"
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
 	nowurl=$(npx now ./output/dist --token=$PUBLIC_NOW_TOKEN --public --name=$name)
@@ -10,27 +10,25 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
 		exit 1
 	fi
 
-	echo "* $name: $nowurl" &>> deployments.txt
+	curl -H "Authorization: token ${PUBLIC_GITHUB_TOKEN}" -H "Content-Type: application/vnd.github.v3+json" -s -X POST "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments" -d '{"body": "This pull request has been deployed to '$nowurl'"}'
 else
 	if [ "$TRAVIS_BRANCH" = "master" ] ; then
-		nowurl=$(npx now ./output/dist --token=$NOW_TOKEN --public --name=$name --scope=dojo --target=production)
+		nowurl=$(npx now ./output/dist --token=$NOW_TOKEN --public --scope=dojo --target=production)
 		if [ "$nowurl" = "" ] ; then
 			echo "Now deployment failed"
 			exit 1
 		fi
 
 		echo "Deployed to $nowurl"
-
-		if [ "$1" = "" ] ; then
-			deploymenturl=$(curl -H "Authorization: Bearer $GITHUB_TOKEN" -H "Content-Type: application/vnd.github.v3+json" -s -X POST https://api.github.com/repos/$TRAVIS_REPO_SLUG/deployments -d '{"ref": "'$TRAVIS_COMMIT'","environment": "production","description": "Deploy request from Travis","auto_merge":false,"required_contexts":[]}' | jq -r '.url')
-			if [ "$deploymenturl" = "null" ] ; then
-				echo "Failed creating github deployment"
-				exit 1
-			fi
-
-			echo "Github deployment url $deploymenturl"
-
-			curl -H "Authorization: Bearer $GITHUB_TOKEN" -H "Content-Type: application/vnd.github.v3+json" -s -X POST $deploymenturl/statuses -d '{"environment": "production", "state": "success", "target_url": "'$nowurl'", "log_url": "'$nowurl'/_logs", "environment_url": "'$nowurl'", "description": "Deployment finished successfully."}'
+		
+		deploymenturl=$(curl -H "Authorization: Bearer $GITHUB_TOKEN" -H "Content-Type: application/vnd.github.v3+json" -s -X POST https://api.github.com/repos/$TRAVIS_REPO_SLUG/deployments -d '{"ref": "'$TRAVIS_COMMIT'","environment": "production","description": "Deploy request from Travis","auto_merge":false,"required_contexts":[]}' | jq -r '.url')
+		if [ "$deploymenturl" = "null" ] ; then
+			echo "Failed creating github deployment"
+			exit 1
 		fi
+
+		echo "Github deployment url $deploymenturl"
+
+		curl -H "Authorization: Bearer $GITHUB_TOKEN" -H "Content-Type: application/vnd.github.v3+json" -s -X POST $deploymenturl/statuses -d '{"environment": "production", "state": "success", "target_url": "'$nowurl'", "log_url": "'$nowurl'/_logs", "environment_url": "'$nowurl'", "description": "Deployment finished successfully."}'
 	fi
 fi
